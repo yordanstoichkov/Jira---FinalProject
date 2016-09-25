f079780265a25e6231adc921ecea1f8c2acd9fff
yordanstoichkov <slav4o95@gmail.com>
2016-09-25 16:54:08 +0300

    DB Connection and DAO added

--- src/controller/DBConnection.java
+++ src/controller/DBConnection.java
@@ -1,5 +1,39 @@
 package controller;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
 
+public class DBConnection {
+	private static Connection connection;
+	private static DBConnection theChosenOneDBConnection;
+
+	private static final String DB_HOSTNAME = "localhost";
+	private static final String DB_PORT = "3306";
+	private static final String DB_USER = "root";
+	private static final String DATABASE = "jira";
+
+	// private static final String DB_PASSWORD = "parolata";
+
+	private static final String DB_PASSWORD = "pecataetup";
+
+	private DBConnection() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			this.connection = DriverManager.getConnection(
+					"jdbc:mysql://" + DB_HOSTNAME + ":" + DB_PORT + "/" + DATABASE, DB_USER, DB_PASSWORD);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public synchronized static DBConnection getInstance() {
+		if (theChosenOneDBConnection == null) {
+			theChosenOneDBConnection = new DBConnection();
+		}
+		return theChosenOneDBConnection;
+	}
+
+	public static Connection getConnection() {
+		return connection;
+	}
 }
--- src/employee/EmployeeDAO.java
+++ src/employee/EmployeeDAO.java
@@ -1,5 +1,73 @@
 package employee;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import controller.DBConnection;
+import exceptions.EmployeeException;
+
 public class EmployeeDAO {
+	private static final String DELETE_USER = "DELETE from users where idUsers = ?;";
+	private static final String REGISTER_USER_TO_DB = "INSERT into users VALUES(NULL,?,?,?,?,md5(?))";
+	private static final String LOGIN_USER = "SELECT idUsers, password FROM users WHERE user_name = ?;";
+
+	public int registerUser(Employee emp) throws EmployeeException {
+		Connection connection = DBConnection.getConnection();
+
+		int id = 0;
+		try {
+			PreparedStatement ps = connection.prepareStatement(REGISTER_USER_TO_DB, Statement.RETURN_GENERATED_KEYS);
+			
+			ps.setString(1, emp.getFirst_name());
+			ps.setString(2, emp.getLast_name());
+			ps.setString(4, emp.getEmail());
+			ps.setString(5, emp.getPassword());
+			ps.executeUpdate();
+			ResultSet rs = ps.getGeneratedKeys();
+			rs.next();
+			id = rs.getInt(1);
+			
+			emp.setEmployeeID(id);
+		} catch (SQLException e) {
+			throw new EmployeeException("User cannot be registered now, please try again later.", e);
+		}
+		return id;
+	}
+
+	public int loginUser(Employee emp) throws EmployeeException {
+		Connection connection = DBConnection.getInstance().getConnection();
+		int id = 0;
+		try {
+			PreparedStatement ps = connection.prepareStatement(LOGIN_USER);
+			ps.setString(1, emp.getUserName());
+			ResultSet rs = ps.executeQuery();
+			rs.next();
+			id = rs.getInt("idUsers");
+			String pass = rs.getString("password");
+			if (!pass.equals(emp.getPassword())) {
+				throw new EmployeeException("Wrong password or username");
+			}
+
+		} catch (SQLException e) {
+			throw new EmployeeException("User cannot be registered now, please try again later.", e);
+		}
+		return id;
+
+	}
+
+	public int removeUser(Employee emp) throws EmployeeException {
+		Connection connection = DBConnection.getConnection();
+		try {
+			PreparedStatement ps = connection.prepareStatement(DELETE_USER);
+			ps.setInt(1, emp.getId());
+			return ps.executeUpdate();
+
+		} catch (SQLException e) {
+			throw new EmployeeException("User cannot be deleted now, please try again later.", e);
+		}
+	}
 
 }
